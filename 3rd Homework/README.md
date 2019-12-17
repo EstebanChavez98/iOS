9ce506438a9a0273cb0c7ea00afbbe31d270248b
EstebanChavez98 <esteban.ch.v95@gmail.com>
2019-12-17 13:56:40 -0700

    Tarea 3

---
+++ 3rd Homework/README.md
@@ -0,0 +1,4 @@
+# Tarea 3
+
+* Se realiza un layout con 6 cuadros, ya sean imagenes o colores. 
+* A la hora de rotar la aplicacion se deben de preservar 6 columnas y 2 renglones. 
---
+++ 3rd Homework/Tarea3/Ejercicio3/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Ejercicio3
+//
+//  Created by CCDM04 on 10/30/19.
+//  Copyright © 2019 AndresFigueroa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
